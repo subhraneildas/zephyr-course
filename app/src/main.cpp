b93bef10d76ad14c36a40b8c9dfdf957484ea68c
Subhraneil <s.das-2@student.utwente.nl>
2026-04-22 10:38:15 +0200

    Homework L4 Task 1

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,9 +3,18 @@
 #include <zephyr/logging/log.h>
 
 //#define BLINK_SLEEP_TIME_MS 1000
-
+//-----------------Playing around with devicetree aliases and nodes for RGB LED-----------------------------
 /* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
+//#define LED_NODE DT_ALIAS(led0)
+/*Now we make a DT_NODE_LABEL reference to our overlay references*/
+//#define LED_NODE DT_NODELABEL(blue_led)
+/*Now we access the green led using the path method although rarely used in production*/
+//#define LED_NODE DT_PATH(leds, led6)
+/*Now lastly we use DT_ALIAS to access a newly created alias my_red_led in my_own.overlay file*/
+//#define LED_NODE DT_ALIAS(my_red_led)
+//-----------------------------------------------------------------------------------------------------------
+//---------------------homework L4 task1---------------------------------------------------------
+#define LED_NODE DT_ALIAS(app_led)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
@@ -24,7 +33,9 @@ int main(void)
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
+        //k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
+        //---------------------homework L4 task1---------------------------------------------------------
+        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
     }
     return 0;
 }
