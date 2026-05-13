115b2ff3f4fab87facb1ce5aea394c5441980981
Subhraneil <s.das-2@student.utwente.nl>
2026-05-13 20:27:34 +0200

    Homework L5 Task 1 - check /hello_world/src/main.cpp & /hello_world/boards/my_board as the custom board used to flash to

--- hello_world/src/main.cpp
+++ hello_world/src/main.cpp
@@ -4,10 +4,26 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <iostream>
-
+//#include <iostream>
+#include <zephyr/kernel.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/drivers/uart.h>
 int main(void)
 {
-	std::cout << "Hello, C++ world! " << CONFIG_BOARD << std::endl;
+ 
+	const struct device *usb_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+
+   if (usb_enable(NULL) != 0) {
+      return -1;
+    }
+
+    /* Wait for DTR signal — host serial monitor has connected */
+    uint32_t dtr = 0;
+    while (!dtr) {
+        uart_line_ctrl_get(usb_dev, UART_LINE_CTRL_DTR, &dtr);
+        k_sleep(K_MSEC(100));
+    }
+	//std::cout << "Hello, C++ world! " << CONFIG_BOARD << std::endl;
+	printk("Hello, C++ world! %s\n", CONFIG_BOARD);	
 	return 0;
 }
