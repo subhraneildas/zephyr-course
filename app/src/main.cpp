1f43011fd3005eab1d99fff895e6db052c4a26e6
Subhraneil <s.das-2@student.utwente.nl>
2026-05-17 11:14:26 +0200

    Intermediate commit for driver development demo. Homework tasks to be pushed in the next commit.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,6 +2,10 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
+
+//----------------------BEFORE Lecture 6 uncomment and check for specific example or homework here-----------------------------
+
+
 //#define BLINK_SLEEP_TIME_MS 1000
 //#define APP_HEARTBEAT_PERIOD_MS 500
 //-----------------Playing around with devicetree aliases and nodes for RGB LED-----------------------------
@@ -15,7 +19,7 @@
 //#define LED_NODE DT_ALIAS(my_red_led)
 //-----------------------------------------------------------------------------------------------------------
 //---------------------homework L4 task1---------------------------------------------------------
-#define LED_NODE DT_ALIAS(app_led)
+/*#define LED_NODE DT_ALIAS(app_led)
 //------------------------------------------------------------------------------------------------
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
@@ -38,6 +42,43 @@ int main(void)
         //k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
         //---------------------homework L4 task1---------------------------------------------------------
         k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
+    }
+    return 0;
+}*/
+//------------------------------------Homeworks or example demos lecture 6 driver dev onwards---------------------------
+#include<zephyr/drivers/sensor.h>
+
+#define SLEEP_TIME_MS 1000
+
+#define LED_NODE DT_ALIAS(led2)
+//#define our_driver0 DT_NODE_LABEL(our_driver0)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+namespace{
+    void test(){
+        const struct device* driver = DEVICE_DT_GET(DT_NODELABEL(our_driver0));
+        struct sensor_value val;
+        auto ret = sensor_channel_get(driver, SENSOR_CHAN_AMBIENT_TEMP, &val);
+        LOG_INF("Channel ret %d",ret);
+    }
+}
+
+int main(void){
+    test();
+    bool led_state = true;
+    if(!gpio_is_ready_dt(&led)) return 0;
+    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    while (1) {
+        if (gpio_pin_toggle_dt(&led) < 0) return 0;
+        led_state = !led_state;
+        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
+        k_msleep(SLEEP_TIME_MS);
+        //k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
+        //---------------------homework L4 task1---------------------------------------------------------
+        //k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
     }
     return 0;
 }
